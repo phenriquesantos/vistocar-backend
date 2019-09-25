2d578e13e292771524a4cf2b19b10fc240b1c302
Luan Santos <501526@soa.ad.pwcinternal.com>
2019-09-25 15:52:29 -0300

    recovery pass

--- resources/user_password_recovery.py
+++ resources/user_password_recovery.py
@@ -9,29 +9,16 @@ from services.email import EmailService
 from uuid import uuid4
 
 PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})"
-MESSAGE_TEMPLATE = """
-Clique no link abaixo para criar uma nova senha:
-
-{0}/#/password-recovery?token={1}&email={2}
-
-Após 10 minutos este link não será mais válido e será necessário iniciar o processo de recuperação de senha novamente.
-"""
 
 class UserPasswordRecoveryResource(Resource):
     def get(self):
         email = request.args['email']
         user = UserModel.get_by_email(email)
 
-        if user:
-            expiration = datetime.now() + timedelta(seconds=600)
-
-            user.recovery = {'token': str(uuid4()), 'expiration': expiration.isoformat()}
-            user.save()
-
-            message = MESSAGE_TEMPLATE.format(environ['ENVIRONMENT_URL'], user.recovery['token'], user.email)
+            message = "email com link de recuperação"
 
             email_service = EmailService()
-            email_service.send(to_address=user.email, message_content=message, subject='D&A in Deals - Recuperação de senha')
+            email_service.send(to_address=user.email, message_content=message, subject='VISTOCAR - Recuperação de senha')
 
             return {'message': 'Recovery message sent'}
 
@@ -41,20 +28,13 @@ class UserPasswordRecoveryResource(Resource):
     def post(self):
         email = request.json.get('email')
         token = request.json.get('token')
-        new_password = request.json.get('password')
         error = None
 
         user = UserModel.get_by_email(email)
 
         # Validations
-        if not user or not user.recovery:
+        if not user:
             error = 'Usuário inválido'
-
-        elif 'token' in user.recovery and user.recovery['token'] != token:
-            error = 'Token inválido'
-
-        elif 'expiration' in user.recovery and datetime.now() > parse(user.recovery['expiration']):
-            error = 'Token expirado'
 
         elif not match(PASSWORD_PATTERN, new_password):
             error = 'A senha não atende os critérios mínimos de complexidade'
@@ -62,7 +42,6 @@ class UserPasswordRecoveryResource(Resource):
         if error:
             return {'message': error}, 400
 
-        user.recovery = None
         user.password = UserModel.generate_hash(new_password)
         user.save()
 
