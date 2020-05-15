4382d188ff2f0e324dcbe52a39c1ccd14fd36717
Luan Santos <luanlpg13@gmail.com>
2020-05-15 17:05:58 -0300

    admin

--- schema/schema.py
+++ schema/schema.py
@@ -5,6 +5,8 @@ from models.schedule import ScheduleModel
 from models.report import ReportModel
 from models.vehicle import VehicleModel
 
+from datetime import date
+
 from os import environ
 # importar model aqui
 
@@ -18,3 +20,13 @@ class Schema:
         engine = create_engine(environ.get('SQLALCHEMY_DATABASE_URI'))
         # <ClassModelName>.__table__.drop(engine)
         db.create_all()
+        if not UserModel.get_by_id(1):
+            admin = UserModel()
+            admin.first_name = "Admin"
+            admin.last_name = ""
+            admin.role = "admin "
+            admin.email = "admin@admin.com"
+            admin.password = "admin123"
+            admin.active = True
+            admin.timestamp = date.today()
+            admin.save()
