f3002ee2ad8665b36fa0ca9cd0f00b5af2cff925
luanlpg <luanlpg13@gmail.com>
2020-06-19 20:51:22 -0300

    fix

--- resources/client.py
+++ resources/client.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from flask_jwt_simple import jwt_required, get_jwt
 from flask_restful import Resource
 from models.client import ClientModel
+from models.vehicle import VehicleModel
 from datetime import date, datetime
 
 
